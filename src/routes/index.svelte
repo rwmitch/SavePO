75f8814c46429fd6856bef48461c43762c43a31d
Rob M <rwmitch@protonmail.com>
2022-07-27 22:45:09 +0100

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,35 @@
 	export const prerender = true;
 </script>
 
-<h1>Test</h1>
-<a href="/Click_and_Drop.pdf">
-  <img src="Click_and_Drop.png" alt="Click and Drop Poster" />
-</a>
+<h1 class="title">#SaveYourPostOffice Resources</h1>
+<ul>
+  <a class="poster" href="/Click_and_Drop.pdf">
+    <img src="Click_and_Drop.png" alt="Click and Drop Poster" />
+  </a>
+</ul>
+
+
+<style>
+  * {
+    font-family: 'Verdana', sans-serif;
+  }
+
+  .title {
+    width: 100%;
+    text-align: center;
+    font-size: 2.5em;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    width: 100vw;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+
+  .poster img {
+    width: calc(100vw / 3 - 1vw);
+  }
+</style>
