569b498850f4fc5fba7acabcbb325614c08e2984
Rob M <rwmitch@protonmail.com>
2022-07-27 22:28:25 +0100

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<h1>Test</h1>
+<a href="/Click_and_Drop.pdf">
+  <img src="Click_and_Drop.png" alt="Click and Drop Poster" />
+</a>
